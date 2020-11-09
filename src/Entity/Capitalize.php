2841d98669208e0d69591e1dd8e197053be7b03b
awet tesfay <whitezscope@gmail.com>
2020-11-09 15:57:19 +0100

    first commit

---
+++ src/Entity/Capitalize.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\Entity;
+
+
+class Capitalize implements Transform
+{
+    public function transform(string $string): string
+    {
+        // TODO: Implement transform() method.
+
+    }
+}
